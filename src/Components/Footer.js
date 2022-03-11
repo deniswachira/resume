4bc3a3fc6142ca36921b59fbdc419283e81bee11
deniswachira <deniswachira77@gmail.com>
2022-03-12 00:04:26 +0300

    inserting favicon

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,7 +23,7 @@ const Footer = ({ data }) => {
             <li>
               Made by{" "}
               <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+                Denis
               </a>
             </li>
           </ul>
