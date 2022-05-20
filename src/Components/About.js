3e5accddb6279f951a0d37e22fab5055f725d78f
deniswachira <deniswachira77@gmail.com>
2022-05-21 01:07:10 +0300

    fixed link

--- src/Components/About.js
+++ src/Components/About.js
@@ -47,7 +47,7 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a href={resumeDownload} className="button">
+                <a href="https://1drv.ms/b/s!Ats52FTl3salgYNxLsHYiMlP30pHSw?e=yvImdA" className="button">
                   <i className="fa fa-download"></i>Download Resume
                 </a>
               </p>
